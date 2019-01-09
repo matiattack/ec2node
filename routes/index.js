c8b331a637a0b8b992119e6a8e3c2a37b9d427b0
Matías Aguirre Luco <matiasaguirreluco@gmail.com>
2019-01-08 22:58:08 -0300

    Se cambia a json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }, null, 3));
 });
 
 module.exports = router;
