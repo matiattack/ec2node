b5f971dfcf8150f16346a5425d2272594b7c2179
Matías Aguirre Luco <matiasaguirreluco@gmail.com>
2019-01-08 22:59:36 -0300

    Se cambia a json

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }, null, 3));
+    res.send(JSON.stringify({ message: "Hola mundo ec2Node" }, null, 3));
 });
 
 module.exports = router;
